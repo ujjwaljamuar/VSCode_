6a6ee852638def25fcffef2de14c1224e158126b
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2021-05-01 11:28:53 +0530

    Delete Python directory

--- Python/mymodule.py
+++
@@ -1,2 +0,0 @@
-def my_func():
-	print('hey i am in my module.py')
--- Python/practice_questions.py/fibonacci.py
+++
@@ -1,8 +0,0 @@
-def gen_fibo(n):
-    a=1
-    b=1
-    for i in range(n):
-        yield a
-        a,b = b,a+b
-for num in gen_fibo(10):
-    print(num)
