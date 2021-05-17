48bbece266099443910d11a5d0208b99dad8850c
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2021-05-17 15:10:12 +0530

    Add files via upload

---
+++ JAVA/AbstractClasses/Eagle.java
@@ -0,0 +1,16 @@
+package Java.AbstractClasses;
+
+public class Eagle extends Bird {
+
+    public Eagle(String name) {
+        super(name);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void fly() {
+        // TODO Auto-generated method stub
+        System.out.println("Fltting from branch to branch.");
+    }
+    
+}
