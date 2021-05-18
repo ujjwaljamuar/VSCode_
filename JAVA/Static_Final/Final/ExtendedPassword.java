5b22dfdc40dd268ed6d4e3c0375643f86c342bac
Ujjwal Jamuar <56722405+ujjwaljamuar@users.noreply.github.com>
2021-05-18 20:22:14 +0530

    Add files via upload

---
+++ JAVA/Static_Final/Final/ExtendedPassword.java
@@ -0,0 +1,16 @@
+package Java.Static_Final.Final;
+
+
+public class ExtendedPassword extends Password {
+    private int decryptedPassword;
+
+    public ExtendedPassword(int password) {
+        super(password);
+        this.decryptedPassword = password;
+    }
+
+//    @Override
+//    public void storePassword() {
+//        System.out.println("Saving password as " + this.decryptedPassword);
+//    }
+}
